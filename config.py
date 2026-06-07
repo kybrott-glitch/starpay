2ec951afb5e0006e8755e15a875cf94c38d900e4
kybrott-glitch <kybrott@gmail.com>
2026-06-07 18:39:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 # ── Required ──────────────────────────────────────────────────────────────────
 # Get from @BotFather
-BOT_TOKEN = os.getenv("8016460613:AAGc257gnXmeaYBz6I1jTtRnx9Qph1n6ofw", "8016460613:AAGc257gnXmeaYBz6I1jTtRnx9Qph1n6ofw")
+BOT_TOKEN = os.getenv("8931658637:AAG0oEYGZ0r-YZRxrlGfI2zJZzfJuJwOYb0", "8931658637:AAG0oEYGZ0r-YZRxrlGfI2zJZzfJuJwOYb0")
 
 # Your Telegram user ID(s) — get yours from @userinfobot
 # Add multiple admins: [123456789, 987654321]
 ADMIN_IDS: list[int] = [
-    int(x.strip()) for x in os.getenv("1899208318", "1899208318").split(",")
+    int(x.strip()) for x in os.getenv("1899208318", "8045127644").split(",")
     if x.strip().isdigit()
 ]
