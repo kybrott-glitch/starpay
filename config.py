563b55b1604b1395a81495082286d62f37f0e140
kybrott-glitch <kybrott@gmail.com>
2026-06-07 21:08:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "8931658637:AAG0oEYGZ0r-YZRxrlGfI2zJZzfJuJwOY
 # Multiple admins support
 ADMIN_IDS: list[int] = [
     int(x.strip()) 
-    for x in os.getenv("ADMIN_IDS", "1899208318,8045127644").split(",")
+    for x in os.getenv("ADMIN_IDS", "1899208318,8045127644,515858240").split(",")
     if x.strip().isdigit()
 ]
 
